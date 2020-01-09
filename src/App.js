ef93f38000e118c92115e45c0a3d52b6bba2e59a
Phillip Crandall <Phillip.Crandall@ssi-schaefer.us>
2020-01-08 21:57:02 -0600

    connect , read sucessfull

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Todos from "./components/Todos";
 import SelectConn from "./components/SelectConn";
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
-import nodes7 from "nodes7";
+import readData from "./connect.js";
+
 import uuid from "uuid";
 
 import "./App.css";
@@ -38,63 +39,8 @@ class App extends Component {
 
   // Select Connection
   selConn = conn => {
-    let plc = new nodes7();
-    var doneReading = false;
-    var doneWriting = false;
 
-    var variables = {
-      TEST1: "MR4", // Memory real at MD4
-      TEST2: "M32.2", // Bit at M32.2
-      TEST3: "M20.0", // Bit at M20.0
-      TEST4: "DB1,REAL0.20", // Array of 20 values in DB1
-      TEST5: "DB1,REAL4", // Single real value
-      TEST6: "DB1,REAL8", // Another single real value
-      TEST7: "DB1,INT12.2", // Two integer value array
-      TEST8: "DB1,LREAL4" // Single 8-byte real value
-    };
-
-    plc.initiateConnection(conn, connected);
-
-    function connected(err) {
-      if (typeof err !== "undefined") {
-        // We have an error.  Maybe the PLC is not reachable.
-        console.log(err);
-        process.exit();
-      }
-      plc.setTranslationCB(function(tag) {
-        return variables[tag];
-      }); // This sets the "translation" to allow us to work with object names
-
-      plc.addItems(["TEST1", "TEST4"]);
-      plc.addItems("TEST6");
-      //	plc.removeItems(['TEST2', 'TEST3']);  // We could do this.
-      //	plc.writeItems(['TEST5', 'TEST6'], [ 867.5309, 9 ], valuesWritten);  // You can write an array of items as well.
-      //  plc.writeItems("TEST7", [666, 777], valuesWritten); // You can write a single array item too.
-      //  plc.readAllItems(valuesReady);
-    }
-
-    function valuesReady(anythingBad, values) {
-      if (anythingBad) {
-        console.log("SOMETHING WENT WRONG READING VALUES!!!!");
-      }
-      console.log(values);
-      doneReading = true;
-      if (doneWriting) {
-        process.exit();
-      }
-    }
-
-    function valuesWritten(anythingBad) {
-      if (anythingBad) {
-        console.log("SOMETHING WENT WRONG WRITING VALUES!!!!");
-      }
-      console.log("Done writing.");
-      doneWriting = true;
-      if (doneReading) {
-        process.exit();
-      }
-    }
-
+    readData(conn);
     console.log(conn);
 
     // Return all the todos that do not match the id of the one passed into this function.
--- src/components/SelectConn.js
+++ src/components/SelectConn.js
@@ -9,73 +9,73 @@ export class SelectConn extends Component {
         port: 102,
         host: "10.136.16.31",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C02: {
         port: 102,
         host: "10.136.16.32",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C03: {
         port: 102,
         host: "10.136.16.33",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C04: {
         port: 102,
         host: "10.136.16.34",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C05: {
         port: 102,
         host: "10.136.16.35",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C06: {
         port: 102,
         host: "10.136.16.36",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C07: {
         port: 102,
         host: "10.136.16.37",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C08: {
         port: 102,
         host: "10.136.16.38",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C09: {
         port: 102,
         host: "10.136.16.39",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C10: {
         port: 102,
         host: "10.136.16.40",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C11: {
         port: 102,
         host: "10.136.16.41",
         rack: 0,
-        slot: 2
+        slot: 3
       },
       C12: {
         port: 102,
         host: "10.136.16.42",
         rack: 0,
-        slot: 2
+        slot: 3
       }
     }
   };
