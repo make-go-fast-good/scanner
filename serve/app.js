3da56dc994d88f310c90ad3c8c783df6c000249b
Phillip Crandall <Phillip.Crandall@ssi-schaefer.us>
2020-01-31 12:51:18 -0600

    added tt39 route

--- serve/app.js
+++ serve/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const tt13Route = require('../plc/routes/TT13/connect');
+const tt39Route = require('../plc/routes/TT39/connect');
 
 app.use('/tt13', tt13Route);
+app.use('/tt39', tt39Route);
 
 module.exports = app;
