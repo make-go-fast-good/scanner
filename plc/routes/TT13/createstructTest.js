6e74c2445007b02a5757aea09617ebe652ef4c9a
pcrandall <crandall.phillip@gmail.com>
2020-01-30 19:44:06 -0600

    class be werkin

--- plc/routes/TT13/createstructTest.js
+++ plc/routes/TT13/createstructTest.js
@@ -1,56 +1,51 @@
-const plcData = createStruct();
+class TT13Row {
+    constructor(_row = 0, _db = "DB1810,", _structLen = 182) {
+        this.row = _row;
+        this.db = _db;
+        this.structLen = _structLen;
+        this.data = {};
+        this.createRow();
+    }
 
-function createStruct() {
-    const db = "DB1810,";
-    let data = {};
-    const rows= {};
-    const structLen = 182
+    createRow() {
+        let offset = this.row * this.structLen;
 
-    for (let i = 0; i < 1; i++) {
-
-        let offset = i * structLen
-
-        createData((6 + offset), 1, "INT")
-        createData((8 + offset), 2, "B", true) //Date
-        createData((10 + offset), 4, "B", true) //Time of day
-        createData((14 + offset), 2, "B", true) //Date
-        createData((16 + offset), 4, "B", true) //Time of day 
-        createData((20 + offset), 5, "X")
-        createData((22 + offset), 8, "X")
-        createData((23 + offset), 8, "X")
-        createData((24 + offset), 5, "INT")
-        createData((36 + offset), 40, "CHAR")
-        createData((76 + offset), 1, "INT")
-        createData((78 + offset), 8, "X")
-        createData((80 + offset), 1, "INT")
-        createData((82 + offset), 8, "X")
-        createData((84 + offset), 8, "X")
-        createData((85 + offset), 1, "X")
-        createData((86 + offset), 6, "INT")
-        createData((98 + offset), 2, "DINT")
-        createData((106 + offset), 8, "X")
-        createData((107 + offset), 3, "X")
-        createData((108 + offset), 3, "INT")
-        createData((114 + offset), 1, "DINT")
-        createData((118 + offset), 6, "B")
-        createData((124 + offset), 8, "X")
-        createData((125 + offset), 1, "CHAR")
-        createData((126 + offset), 8, "X")
-        createData((127 + offset), 8, "X")
-        createData((128 + offset), 8, "X")
-        createData((129 + offset), 8, "X")
-        createData((130 + offset), 21, "INT")
-        createData((172 + offset), 1, "WORD")
-        createData((174 + offset), 7, "INT")
-
-        rows[i] = data;
-
-        data = {};
+        this.createData((6 + offset), 1, "INT")
+        this.createData((8 + offset), 2, "B", true) //Date
+        this.createData((10 + offset), 4, "B", true) //Time of day
+        this.createData((14 + offset), 2, "B", true) //Date
+        this.createData((16 + offset), 4, "B", true) //Time of day 
+        this.createData((20 + offset), 5, "X")
+        this.createData((22 + offset), 8, "X")
+        this.createData((23 + offset), 8, "X")
+        this.createData((24 + offset), 5, "INT")
+        this.createData((36 + offset), 40, "CHAR")
+        this.createData((76 + offset), 1, "INT")
+        this.createData((78 + offset), 8, "X")
+        this.createData((80 + offset), 1, "INT")
+        this.createData((82 + offset), 8, "X")
+        this.createData((84 + offset), 8, "X")
+        this.createData((85 + offset), 1, "X")
+        this.createData((86 + offset), 6, "INT")
+        this.createData((98 + offset), 2, "DINT")
+        this.createData((106 + offset), 8, "X")
+        this.createData((107 + offset), 3, "X")
+        this.createData((108 + offset), 3, "INT")
+        this.createData((114 + offset), 1, "DINT")
+        this.createData((118 + offset), 6, "B")
+        this.createData((124 + offset), 8, "X")
+        this.createData((125 + offset), 1, "CHAR")
+        this.createData((126 + offset), 8, "X")
+        this.createData((127 + offset), 8, "X")
+        this.createData((128 + offset), 8, "X")
+        this.createData((129 + offset), 8, "X")
+        this.createData((130 + offset), 21, "INT")
+        this.createData((172 + offset), 1, "WORD")
+        this.createData((174 + offset), 7, "INT")
 
     }
 
-    // take four parameters(start, length, type, array)
-    function createData(s, l, t, a) {
+    createData(s, l, t, a) {
         // b for byte
         let b;
         switch (t) {
@@ -74,17 +69,20 @@ function createStruct() {
             if (i !== 0 && !a) s += b;
             // If a is true we are working with a byte array, break from loop , we will decode later 
             if (a) {
-                data[s] = (db + t + s + "." + l.toString())
+                this.data[s + ".0"] = (this.db + t + s + "." + l.toString())
                 break;
             }
             if (b === .1) {
-                data[s.toFixed(1)] = (db + t + s.toFixed(1));
+                this.data[s.toFixed(1)] = (this.db + t + s.toFixed(1));
             } else {
-                data[s] = (db + t + s)
+                this.data[s + ".0"] = (this.db + t + s)
             }
         }
     }
-    return rows;
 }
 
-module.exports = plcData;
+let testRow = new TT13Row();
+
+console.log(testRow);
+
+module.exports = testRow;
