1fe2ccb359a612310f37d75505a068181bfc4dcf
pcrandall <crandall.phillip@gmail.com>
2020-01-31 12:34:08 -0600

    cleaned up.

--- plc/routes/TT13/connect.js
+++ plc/routes/TT13/connect.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const nodes7 = require("nodes7");
 const plc = new nodes7();
 class dataRow {
-    constructor() {
+    constructor(_row = 0) {
+        this.row = _row;
         this.data = {};
     }
 }
@@ -14,6 +15,7 @@ function readData() {
         let data;
 
         // grab the connection
+        //TODO get connection object from the front end
         plc.initiateConnection({
             port: 102,
             host: "10.136.16.31",
@@ -29,14 +31,17 @@ function readData() {
             }
 
             // This sets the "translation" to allow us to work with object names
-            plc.setTranslationCB(function (tag) {
-                return variables[tag];
+            plc.setTranslationCB(tag => {
+                return variables[tag]
             });
 
+            // Add items to the interal reading polling list.
             plc.addItems(Object.keys(variables));
 
+            // Read items then return a values object.
             plc.readAllItems(callback)
 
+            //Callback function from readAllItems method.
             async function callback(err, values) {
                 if (err) {
                     console.log("SOMETHING WENT WRONG READING VALUES!!!!");
@@ -47,26 +52,25 @@ function readData() {
                 //Since data is one big object we want to grab all the keys to use array functions (36400 keys)
                 const dataKeys = Object.keys(data);
 
-                let retData = {};
+                let plcData = {};
 
                 for (i = 0; i < 200; i++) {
-
                     //lets construct our object
-                    retData[i] = new dataRow(i);
+                    plcData[i] = new dataRow(i);
                     //row is an array of the raw object keys from the callback function
-                    let row = dataKeys.filter(function (val) {
+                    let row = dataKeys.filter(val => {
                         return (parseFloat(val) <= ((i * 182) + 186)) && (parseFloat(val) >= (6 + (i * 182)))
                     })
-                    //iterate throught he dataKeys array and create a sensible structure
-                    row.forEach((key) => {
+                    //iterate through the dataKeys array and create a sensible structure
+                    row.forEach(key => {
                         item = parseFloat(key) - (i * 182)
-                        retData[i].data[item.toFixed(1)] = data[key];
+                        plcData[i].data[item.toFixed(1)] = data[key];
                     })
 
                 }
-
-                resolve(retData);
-                //Drop the connection, to fix all the things. 
+                    //Return the plcData object and resolve the promise.
+                resolve(plcData);
+                    //Drop the connection, to fix all the things. 
                 plc.dropConnection();
             }
         }
@@ -84,8 +88,6 @@ const awaitHandlerFactory = (middleware) => {
 }
 
 router.get('/', awaitHandlerFactory(async (request, response) => {
-    //console.log("request: \n");
-    //console.log(request);
     //let conn = request.query.conn
     readData().then(data => {
         response.send(data)
