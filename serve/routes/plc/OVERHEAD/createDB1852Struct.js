3564f940e9c6667546bcd9a7b73e5554c8ea3c93
pcrandall <crandall.phillip@gmail.com>
2020-03-28 08:24:06 -0500

    keys and structs

--- serve/routes/plc/OVERHEAD/createDB1852Struct.js
+++ serve/routes/plc/OVERHEAD/createDB1852Struct.js
@@ -7,17 +7,17 @@ function createStruct() {
 
         let offset = i * structLen
 
-        createData((0 + offset), 1, "X")
-        createData((2 + offset), 1, "INT")
-        createData((4 + offset), 1, "B")
-        createData((5 + offset), 1, "B")
-        createData((6 + offset), 1, "B") 
-        createData((7 + offset), 1, "B") 
-        createData((8 + offset), 1, "WORD") 
-        createData((10 + offset), 1, "INT") 
-        createData((12 + offset), 8, "X")
-        createData((13 + offset), 8, "X")
-        createData((14 + offset), 20, "CHAR")
+        createData((0  + offset), 1,   "X")
+        createData((2  + offset), 1,   "INT")
+        createData((4  + offset), 1,   "B")
+        createData((5  + offset), 1,   "B")
+        createData((6  + offset), 1,   "B")
+        createData((7  + offset), 1,   "B")
+        createData((8  + offset), 1,   "WORD")
+        createData((10 + offset), 1,   "INT")
+        createData((12 + offset), 8,   "X")
+        createData((13 + offset), 8,   "X")
+        createData((14 + offset), 20,  "CHAR")
         createData((34 + offset), 170, "CHAR")
    }
 
