814c40b3db45df575fa1437523a33f4c4a2194e9
pcrandall <crandall.phillip@gmail.com>
2020-03-30 07:37:29 -0500

    Process data

--- serve/routes/plc/OVERHEAD/processDB171Data.js
+++ serve/routes/plc/OVERHEAD/processDB171Data.js
@@ -15,32 +15,30 @@ const process = function processPlcData(data) {
   //data is one big object we want to grab all the keys to use array functions
   const dataKeys = Object.keys(data);
 
-    //Add one so the index starts at one in the table.
     let plcData = new dataRow(1);
-    let row = [];
 
     //iterate through the dataKeys array and create a sensible structure
     let barcode = [];
     let shipping_label = [];
 
-    row.forEach((key, index) => {
+    dataKeys.forEach((key, index) => {
       // change from boolean to string representation so the data table can read.
       if (data[key] === true) data[key] = "true";
       if (data[key] === false) data[key] = "false";
-      if (index >= 57 && index <= 97) {
+      if (index >= 56 && index <= 95) {
         // build a string from the char array
         plcData.barcode += data[key];
       }
 
-      if (index >= 164 && index <= 363) {
+      if (index >= 163) {
         // build a string from the char array
         plcData.shipping_label += data[key];
       }
        // normal key
         plcData.data[Keys[index]] = data[key];
       //remove the white space
-      plcData.barcode = plcData[i].barcode.trim();
-      plcData.shipping_label = plcData[i].shipping_label.trim();
+      plcData.barcode = plcData.barcode.trim();
+      plcData.shipping_label = plcData.shipping_label.trim();
     });
     console.log(plcData)
 
