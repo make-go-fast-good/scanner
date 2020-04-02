f607988c2c48008f60b49de7c63aa90d0c46344c
pcrandall <crandall.phillip@gmail.com>
2020-04-02 07:29:36 -0500

    Front end columns

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import MaterialTable from "material-table";
 import DefaultTable from "./DefaultTable";
 import TT13Columns from "../config/TT13Columns";
-import DB171Columns from "../config/DB1852Columns";
-import DB421Columns from "../config/DB1852Columns";
+import DB171Columns from "../config/DB171Columns";
+import DB421Columns from "../config/DB421Columns";
 import DB1852Columns from "../config/DB1852Columns";
 import ERRORColumns from "../config/ERRORColumns";
 export class Table extends Component {
@@ -14,9 +14,37 @@ export class Table extends Component {
     let _columns;
     let tableColumns;
 
+      switch (this.props.type) {
+          case 'TT13':
+              tableColumns = TT13Columns
+              break;
+          case 'ERROR':
+              tableColumns = ERRORColumns
+              break;
+          case 'OVERHEAD':
+              switch (this.props.area) {
+                  case 'C08':
+                      tableColumns = DB1852Columns
+                      break;
+                   case 'C09':
+                   case 'C10':
+                   case 'C12':
+                      tableColumns = DB421Columns
+                      break;
+                   case 'C11':
+                      tableColumns = DB171Columns
+                      break;
+                  default:
+                      tableColumns = DB1852Columns
+              }
+          default:
+              tableColumns = TT13Columns
+      }
+/*
     if(this.props.type === 'TT13') tableColumns = TT13Columns
     if(this.props.type === 'OVERHEAD') tableColumns = DB1852Columns
     if(this.props.type === 'ERROR') tableColumns = ERRORColumns
+*/
 
     if (this.props.loading === true) {
       return <DefaultTable loading={this.props.loading} css={this.props.css} />;
