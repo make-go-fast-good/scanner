437868d5480d75c901ad802d4f228740306a6272
pcrandall <crandall.phillip@gmail.com>
2020-07-05 16:51:30 -0500

    good enough

--- serve/app.js
+++ serve/app.js
@@ -7,12 +7,14 @@ const overheadData = require('./routes/plc/OVERHEAD/connect');
 const errorData    = require('./routes/plc/ERROR/connect');
 const coordinates  = require('./routes/coordinates/connect');
 const scanners     = require('./routes/plc/SCANNERS/connect');
+const bookmarks    = require('./routes/bookmarksflt/connect');
 
-app.use('/TT13/connect',     tt13Data);
-app.use('/OVERHEAD/connect', overheadData);
-app.use('/ERROR/connect',    errorData);
-app.use('/SCANNERS/connect', scanners);
-app.use('/COORDINATES',      coordinates);
+app.use('/TT13/connect',      tt13Data);
+app.use('/OVERHEAD/connect',  overheadData);
+app.use('/ERROR/connect',     errorData);
+app.use('/SCANNERS/connect',  scanners);
+app.use('/COORDINATES',       coordinates);
+app.use('/BOOKMARKS/connect', bookmarks);
 
 /*
 app.use(express.static('./routes/coordinates/public'));
